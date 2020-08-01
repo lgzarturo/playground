c6e7f72672aa01a2345fbe3b31ec1e749c5f12fe
Arturo López <lgzarturo@gmail.com>
2020-08-01 03:19:52 -0500

    Instalando express e iniciando la aplicación

---
+++ javascript/node/es6-node-exercises/app.js
@@ -0,0 +1,41 @@
+const debug = require('debug')('app')
+const colors = require('colors')
+const { listTable, createMultiplyTable } = require('./app/multiply')
+const { getWeather } = require('./app/weather')
+
+const logger = require('./config/logger')
+const vargs = require('./config/yargs').vargs
+
+debug('Comandos %o', vargs)
+
+let command = vargs._[0]
+
+debug('Comando seleccionado %o', command)
+
+switch (command) {
+  case 'list':
+    logger.info('List command : listar en consola la tabla de multiplicar')
+    listTable(vargs.base, vargs.limit)
+    break
+  case 'create':
+    logger.info('Create command: Generar un archivo con la tabla de multiplicar')
+    createMultiplyTable(vargs.base, vargs.limit)
+      .then((file) => logger.success(`Archivo creado: ${colors.inverse(file)}`))
+      .catch((err) => logger.error(err))
+    break
+  case 'todo':
+    logger.info('Crear una lista de todo')
+    break
+  case 'complete':
+    logger.info('Completar una tarea')
+    break
+  case 'weather':
+    logger.info('Clima de la ciudad')
+    getWeather(vargs.city)
+      .then((res) => logger.success(`Datos`, {data:res}))
+      .catch((err) => logger.error(err))
+    break
+  default:
+    logger.error('El comando no es valido')
+    break
+}
