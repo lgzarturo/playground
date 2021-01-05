726cd196327db4db6ccda98066da1e59f8c03a77
Arturo López <lgzarturo@gmail.com>
2021-01-05 01:29:06 -0500

    Async y await

--- dart/basic_examples/bin/futures.dart
+++ dart/basic_examples/bin/futures.dart
@@ -1,11 +1,22 @@
-void main() {
+void main() async {
   print('Ciclo de vida');
   httpGet('https://arthurolg.com').then((data) => print(data));
+  print('Fin del programa');
+
+  print('Nuevo ciclo de vida');
+  String data = await httpGetAwait('https://webdeveloperhints.com');
+  print(data);
   print('Fin del programa');
 }
 
 Future<String> httpGet(String url) {
   return Future.delayed(Duration(seconds: 4), () {
-    return 'Hola mundo';
+    return 'Furute -> Hola mundo ';
+  });
+}
+
+Future<String> httpGetAwait(String url) {
+  return Future.delayed(Duration(seconds: 4), () {
+    return 'Async Await -> Hola mundo Async';
   });
 }
