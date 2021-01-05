d00d0a2057f1743167d900d677d15a0723971dda
Arturo López <lgzarturo@gmail.com>
2021-01-04 20:38:32 -0500

    proyecto con dart, prácticas con variables

---
+++ dart/basic_examples/bin/basic_examples.dart
@@ -0,0 +1,8 @@
+import 'package:basic_examples/basic_examples.dart' as basic_examples;
+
+void main(List<String> arguments) {
+  var name = 'Arturo López';
+  var interval = 10;
+  var pi = 3.141592;
+  print('Hello $name - ${interval * pi}: ${basic_examples.calculate()}!');
+}
