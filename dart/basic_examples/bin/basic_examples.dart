757f98b8648afd3d59085610e46f0e78ebd0dc00
Arturo López <lgzarturo@gmail.com>
2021-01-04 20:50:13 -0500

    Configurando el entorno

--- dart/basic_examples/bin/basic_examples.dart
+++ dart/basic_examples/bin/basic_examples.dart
@@ -1,8 +1,25 @@
 import 'package:basic_examples/basic_examples.dart' as basic_examples;
+import 'package:test/test.dart';
 
 void main(List<String> arguments) {
-  var name = 'Arturo López';
-  var interval = 10;
-  var pi = 3.141592;
+  String name = 'Arturo López';
+  int interval = 10;
+  double pi = 3.141592;
   print('Hello $name - ${interval * pi}: ${basic_examples.calculate()}!');
+
+  String message = 'Comillas simples';
+
+  print(message[0]);
+  print(message[message.length - 1]);
+
+  bool isActive = true;
+  if (isActive) {
+    print('La opción esta activada');
+  }
+
+  isActive = !isActive;
+
+  if (!isActive) {
+    print('Esta desactivado');
+  }
 }
