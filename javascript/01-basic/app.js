21bf9b86d3f7ab7ef9bc73f2d83e25245c6eb3fd
Arturo López <lgzarturo@gmail.com>
2021-02-20 13:26:42 -0500

    Operaciones básicas

--- javascript/01-basic/app.js
+++ javascript/01-basic/app.js
@@ -3,15 +3,6 @@ Código de JavaScript básico
 */
 console.log('Enlazando archivo externo');
 
-// Obtener el nombre desde el navegador
-var name = prompt('Cual es tu nombre?');
-if (name === '' || name === 'null') {
-    name = 'mundo';
-}
-const greetings = `Hola, ${name}!`;
-alert(greetings);
-document.querySelector('#name').innerHTML = greetings;
-
 /*
 Variables 
 */
@@ -22,10 +13,6 @@ console.log(iProduct);
 
 // Cambiando el valor de la variable
 iProduct = 'iMac';
-console.log(iProduct);
-
-// Asignando otro tipo de dato a la variable
-iProduct = 25000;
 console.log(iProduct);
 
 // Iniciar una variable sin valor
@@ -46,3 +33,45 @@ const price = 300.00;
 console.log(`El precio de la constante es $${price}`);
 
 console.log(new String('Cadena de texto desde la instancia de la clase String()'));
+
+document.querySelector('#greeting').addEventListener('click', () => {
+    // Obtener el nombre desde el navegador
+    var name = prompt('Cual es tu nombre?');
+    if (name === '' || name === 'null') {
+        name = 'mundo';
+    }
+    const greetings = `Hola, ${name}!`;
+    alert(greetings);
+    document.querySelector('#name').innerHTML = greetings;
+});
+
+document.querySelector('#calculate').addEventListener('click', () => {
+    // Asignando otro tipo de dato a la variable
+    let iProduct = 25000;
+    let discount = 20;
+    let random = Math.ceil(Math.random() * 30);
+    
+    let result = {
+        name: 'iProduct',
+        price: `$${iProduct} MXN`,
+        discount: `$${iProduct * (discount/100)}`,
+        total: `$${iProduct - (iProduct * (discount/100))}`,
+        addCost: `$${iProduct * (1 + (discount/100))}`,
+        random: random,
+        number: random % 2
+    }
+
+    let message = `
+        <strong>${result.name}</strong>
+        <ul>
+            <li>Price: ${result.price}</li>
+            <li>${discount}% del iProduct: ${result.discount}</li>
+            <li>Restamos el ${discount}% al iProduct: ${result.total}</li>
+            <li>Sumamos el ${discount}% del iProduct: ${result.addCost}</li>
+            <li>Número aleatorio: ${result.random}</li>
+            <li>${result.random} es un número ${(result.number == 0) ? 'par': 'impar'}</li>
+        </ul>
+    `
+    console.log(result);
+    document.querySelector('#result').innerHTML = message;
+});
