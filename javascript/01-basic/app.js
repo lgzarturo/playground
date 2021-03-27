07c731925c8864dc0cb5452bb4acde75d1b3a322
Arturo López <lgzarturo@gmail.com>
2021-03-26 21:25:55 -0500

    👨🏻‍💻 Prácticas con funciones y parametros por default

--- javascript/01-basic/app.js
+++ javascript/01-basic/app.js
@@ -8,7 +8,7 @@ Variables
 */
 
 // Asignando un valor a la variable
-var iProduct = 'iPhone 12'
+let iProduct = 'iPhone 12'
 console.log(iProduct)
 
 // Cambiando el valor de la variable
@@ -16,27 +16,28 @@ iProduct = 'iMac'
 console.log(iProduct)
 
 // Iniciar una variable sin valor
-var isActive
+let isActive
 isActive = false
+// eslint-disable-next-line no-unused-vars
 isActive = true
 
 // Iniciar multiples variables
-var color = '#fff',
-  title = 'Website',
-  score = 5.0
+const color = '#fff'
+const title = 'Website'
+const score = 5.0
 console.log({ color, title, score })
 
-let jedi = 'Grogu'
+const jedi = 'Grogu'
 console.log(`${jedi} será jedi?`)
 
 const price = 300.0
 console.log(`El precio de la constante es $${price}`)
 
-console.log(new String('Cadena de texto desde la instancia de la clase String()'))
+console.log('Cadena de texto desde la instancia de la clase String()')
 
 document.querySelector('#greeting').addEventListener('click', () => {
   // Obtener el nombre desde el navegador
-  var name = prompt('Cual es tu nombre?')
+  let name = prompt('Cual es tu nombre?')
   if (name === '' || name === 'null') {
     name = 'mundo'
   }
@@ -47,11 +48,11 @@ document.querySelector('#greeting').addEventListener('click', () => {
 
 document.querySelector('#calculate').addEventListener('click', () => {
   // Asignando otro tipo de dato a la variable
-  let iProduct = 25000
-  let discount = 20
-  let random = Math.ceil(Math.random() * 30)
+  const iProduct = 25000
+  const discount = 20
+  const random = Math.ceil(Math.random() * 30)
 
-  let result = {
+  const result = {
     name: 'iProduct',
     price: `$${iProduct} MXN`,
     discount: `$${iProduct * (discount / 100)}`,
@@ -61,7 +62,7 @@ document.querySelector('#calculate').addEventListener('click', () => {
     number: random % 2,
   }
 
-  let message = `
+  const message = `
         <strong>${result.name}</strong>
         <ul>
             <li>Price: ${result.price}</li>
@@ -69,7 +70,7 @@ document.querySelector('#calculate').addEventListener('click', () => {
             <li>Restamos el ${discount}% al iProduct: ${result.total}</li>
             <li>Sumamos el ${discount}% del iProduct: ${result.addCost}</li>
             <li>Número aleatorio: ${result.random}</li>
-            <li>${result.random} es un número ${result.number == 0 ? 'par' : 'impar'}</li>
+            <li>${result.random} es un número ${result.number === 0 ? 'par' : 'impar'}</li>
         </ul>
     `
   console.log(result)
