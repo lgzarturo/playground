7902cb3fdcd5b4a69ab193777f010709cd8faeba
ArturoLG <lgzarturo@gmail.com>
2021-10-29 12:44:51 -0500

    Practicas con spring boot

---
+++ springboot/fundamentos/src/main/groovy/com/playground/alg/fundamentos/FundamentosApplication.groovy
@@ -0,0 +1,13 @@
+package com.playground.alg.fundamentos
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FundamentosApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication, args)
+	}
+
+}
