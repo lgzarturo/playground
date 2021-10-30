02942a14fe797e0454413a85cd090908a6e966dc
ArturoLG <lgzarturo@gmail.com>
2021-10-29 22:38:52 -0500

    Configuración de base de datos con archivo properties

--- springboot/fundamentos/src/main/groovy/com/playground/alg/fundamentos/configuration/ConfigurationBean.groovy
+++ springboot/fundamentos/src/main/groovy/com/playground/alg/fundamentos/configuration/ConfigurationBean.groovy
@@ -6,14 +6,26 @@ import com.playground.alg.fundamentos.bean.Custom2BeanImplement
 import com.playground.alg.fundamentos.bean.OperationDependency
 import com.playground.alg.fundamentos.bean.OperationImplement
 import com.playground.alg.fundamentos.bean.UnBeanDependency
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 
 import javax.sql.DataSource
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 class ConfigurationBean {
+    @Value("\${db.url}")
+    private String jdbcUrl
+    @Value("\${db.driver}")
+    private String driver
+    @Value("\${db.username}")
+    private String username
+    @Value("\${db.password}")
+    private String password
+
     @Bean
     UnBeanDependency beanDependency() {
         return new Custom2BeanImplement()
@@ -32,10 +44,10 @@ class ConfigurationBean {
    @Bean
     DataSource dataSource() {
        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create()
-       dataSourceBuilder.driverClassName("org.h2.Driver")
-       dataSourceBuilder.url("jdbc:h2:mem:testdb")
-       dataSourceBuilder.username("sa")
-       dataSourceBuilder.password("")
+       dataSourceBuilder.driverClassName(driver)
+       dataSourceBuilder.url(jdbcUrl)
+       dataSourceBuilder.username(username)
+       dataSourceBuilder.password(password)
        dataSourceBuilder.build()
    }
 }
