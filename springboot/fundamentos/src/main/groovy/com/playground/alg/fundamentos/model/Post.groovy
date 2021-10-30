71c8db7328e1613921d8481c0036def0c8b999c1
ArturoLG <lgzarturo@gmail.com>
2021-10-29 21:13:55 -0500

    Modelando entidades con JPA

---
+++ springboot/fundamentos/src/main/groovy/com/playground/alg/fundamentos/model/Post.groovy
@@ -0,0 +1,22 @@
+package com.playground.alg.fundamentos.model
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table
+class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    Long id
+    @Column(length = 200)
+    String description
+    @ManyToOne
+    User user
+}
---
+++ springboot/fundamentos/src/main/groovy/com/playground/alg/fundamentos/model/User.groovy
@@ -0,0 +1,32 @@
+package com.playground.alg.fundamentos.model
+
+import com.fasterxml.jackson.annotation.JsonManagedReference
+
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.OneToMany
+import javax.persistence.Table
+import java.time.LocalDate
+
+@Entity
+@Table
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
+    Long id
+    @Column(unique = true, length = 64)
+    String username
+    @Column(unique = true, length = 200)
+    String email
+    String password
+    LocalDate birthDate
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    List<Post> posts = []
+}
