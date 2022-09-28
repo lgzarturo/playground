3048989b94fbdce2269274f15e90dee983f2dc49
Arturo López <lgzarturo@gmail.com>
2022-09-28 02:14:51 -0500

    Estableciendo la versión de node y de npm
    Se hace uso de la propiedad "engines" de package.json para definir las versiones de node y de npm.
    También se especifica en el archivo .npmrc que se usarán la versiones definidas para la ejecución del proyecto.

--- react/exercises/src/App.jsx
+++ react/exercises/src/App.jsx
@@ -3,15 +3,15 @@ import Countdown from './components/Countdown'
 import Counter from './components/Counter'
 import PresentationCard from './components/PresentationCard'
 
-function App() {
-  return (
-    <div className="App">
-          <h1>Hola mundo</h1>
-          <Countdown />
-          <PresentationCard />
-          <Counter />
-    </div>
-  )
+function App () {
+    return (
+        <div className="App">
+            <h1>Hola mundo</h1>
+            <Countdown />
+            <PresentationCard />
+            <Counter />
+        </div>
+    )
 }
 
 export default App
